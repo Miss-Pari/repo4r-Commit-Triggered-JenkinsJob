80ca328e17b31d7961e7241878a74d04428ff1f9
Prapti Ajgaonkar <100202405+Miss-Pari@users.noreply.github.com>
2024-07-25 11:18:23 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -2,12 +2,14 @@ public class Reverse {
     public static void main(String[] args) {
         
         int num = 1234567, reversed = 0;
-    
+
+        System.out.println("Original Number String: " + num);
+        
         for(;num != 0; num /= 10) {
         int digit = num % 10;
         reversed = reversed * 10 + digit;
         }
     
-        System.out.println("Reversed Number: " + reversed);
+        System.out.println("Reversed Number String: " + reversed);
     }
 }
